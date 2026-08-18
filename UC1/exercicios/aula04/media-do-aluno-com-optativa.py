06fdcbfd4e88a13b50e36f8e70d45546c17b90d7
PHELLIPE LISBOA BARBOSA <phellipe.barbosa@aluno.senac.rio>
2026-08-17 21:36:48 -0300

    Finalizando o exercício 5 da aula 4

--- UC1/exercicios/aula04/media-do-aluno-com-optativa.py
+++ UC1/exercicios/aula04/media-do-aluno-com-optativa.py
@@ -13,3 +13,34 @@ Observação: nota optativa - o estudante decide fazer uma prova extra para melh
 resultado final.
 '''
 
+try:
+    print("==========================================================")
+    first_grade = float(input("Insira a nota da primeira avaliação: "))
+    second_grade = float(input("Insira a nota da segunda avaliação: "))
+    optative_grade = float(input("Insira a nota da avaliação optativa (Digite -1 caso o aluno não tenha feito): "))
+    print("==========================================================")
+
+    if first_grade < 0 or first_grade > 10 or second_grade < 0 or second_grade > 10:
+        raise ValueError("Insira valores válidos para as notas (0 - 10).")
+
+    if optative_grade != -1:
+        if first_grade < second_grade and optative_grade > first_grade:
+            first_grade = optative_grade
+        elif second_grade < first_grade and optative_grade > second_grade:
+            second_grade = optative_grade
+
+        average = (first_grade + second_grade) / 2 
+    else:
+        average = (first_grade + second_grade) / 2 
+
+    if average < 3:
+        status = "Reprovado :("
+    elif average < 6:
+        status = "Recuperação :("
+    else: 
+        status = "Aprovado :)"
+
+    print(f"Média: {average:.2f} | Situação: {status}")
+    print("==========================================================")
+except ValueError as error:
+    print(f"Entrada inválida: {error}")
