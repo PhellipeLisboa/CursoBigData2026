d598ad929cc4b5a7a7dfd0f4f4c84859ce7b3a8b
PHELLIPE LISBOA BARBOSA <phellipelisboa.dev@gmail.com>
2026-08-21 21:26:25 -0300

    Adiciona arquivos de aula do dia 21/08/2026

--- UC1/aula05/aula05-19082026.py
+++ UC1/aula05/aula05-19082026.py
@@ -6,5 +6,63 @@
 # for i in meunome:
 #     print(i)
 
-for i in range(10):
-    print(i)
+# for i in range(100, 10, -2):
+#     print(i)
+
+# WHILE
+
+# somador = int(input("Registro: "))
+# controle = 0
+
+# while controle <= 30:
+#     controle += somador 
+#     somador = int(input("Registro: "))
+
+# print("Oficina lotada!")
+
+
+# for i in range(5):
+#     try:
+#         print(f"Número {i + 1} de 5:")
+#         num = float(input("Digite um número: "))
+#         dobro = num * 2
+#         triplo = num * 3
+#         quádruplo = num * 4
+#         print(f" Resultado: Dobro={dobro}, Triplo={triplo}, Quádruplo={quádruplo}\n")
+#     except ValueError:
+#         print("Entrada inválida. Tente novamente.")
+#         num = float(input("Digite um número: "))
+
+# contador = 0
+
+# while contador < 5:
+#     try:
+#         print(f"Número {contador + 1} de 5:")
+#         num = float(input("Digite um número: "))
+#         dobro = num * 2
+#         triplo = num * 3
+#         quádruplo = num * 4
+#         print(f" Resultado: Dobro={dobro}, Triplo={triplo}, Quádruplo={quádruplo}\n")
+#         contador += 1
+#     except ValueError:
+#         print("Entrada inválida. Tente novamente.")
+
+print("--- Usando WHILE (Repetição Condicional) ---")
+contador = 0 
+limite = 5 
+
+while True: 
+
+    if contador >= limite:
+        break
+
+    try:
+        print(f"Número {contador + 1} de {limite}:")
+        num = float(input("Digite um número: "))
+        dobro = num * 2
+        triplo = num * 3
+        quádruplo = num * 4
+        print(f" Resultado: Dobro={dobro}, Triplo={triplo}, Quádruplo={quádruplo}\n")
+        contador += 1 
+    except ValueError:
+        print("Entrada inválida. Tente novamente.")
