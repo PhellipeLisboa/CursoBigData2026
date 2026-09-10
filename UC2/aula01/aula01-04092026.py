30d9c08b215981c96a5ca89c0ec731984a2a9c23
PHELLIPE LISBOA BARBOSA <phellipe.barbosa@aluno.senac.rio>
2026-09-09 21:46:39 -0300

    Adiciona os arquivos da aula do dia 09/09/2026

--- UC2/aula01/aula01-04092026.py
+++ UC2/aula01/aula01-04092026.py
@@ -6,15 +6,15 @@
 import pandas as pd
 import numpy as np
 
-numeros_impares = [43, 55, 1, 3, 11, 27, 109]
-numeros_seq = [2, 3, 4, 5, 6, 6, 7]
+# numeros_impares = [43, 55, 1, 3, 11, 27, 109]
+# numeros_seq = [2, 3, 4, 5, 6, 6, 7]
 
-print(type(numeros_impares))
+# print(type(numeros_impares))
 
 # serie_teste = pd.Series(numeros_impares, numeros_seq)
 # print(serie_teste)
 
-serie_impares = pd.Series(numeros_impares)
+# serie_impares = pd.Series(numeros_impares)
 # print(serie_impares)
 # print(type(serie_impares))
 
@@ -28,5 +28,30 @@ serie_impares = pd.Series(numeros_impares)
 # print(serie_impares > 50)
 # print(serie_impares[serie_impares > 50])
 
-serie2_impares = pd.Series(numeros_impares, index = ['a', 'b', 'c', 'd', 'e', 'f', 'g'])
-print(serie2_impares)
+# serie2_impares = pd.Series(numeros_impares, index = ['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+# print(serie2_impares)
+
+# filmes = {
+#     'título': ["Lagoa Azul", "Agente Secreto", "Gênio Indomável"],
+#     'categoria': ["Romance", "Ação", "Drama"],
+#     'ano': ["1980", "2025", "1997"]
+# }
+
+# tabela_filmes = pd.DataFrame(filmes)
+
+# print(filmes)
+# print(type(filmes))
+
+# print(tabela_filmes)
+# print(type(tabela_filmes))
+
+# print(serie_impares)
+
+# quadrado_serie_impares = serie_impares * serie_impares
+
+# print(quadrado_serie_impares)
+
+## Leitura de arquivos XLSX
+
+leitura_invest = pd.read_excel('../exercicios/aula01/base_invest.xlsx', sheet_name='Participante')
+print(leitura_invest.head())
---
+++ UC2/aula02/aula02-09092026.py
@@ -0,0 +1,32 @@
+# Aula 02 - Dia 09/09/2026
+# Tema principal: 
+
+# LOC
+# ILOC
+# QUERY
+
+import pandas as pd
+import numpy as np
+
+filmes = {
+    'título': ["Lagoa Azul", "Agente Secreto", "Gênio Indomável", "A Freira", "Brinquedo Assassino", "Top Gun"],
+    'categoria': ["Romance", "Ação", "Drama", "Terror", "Comédia", "Aventura"],
+    'ano': ["1980", "2025", "1997", "2022", "1995", "1986"],
+    'faturamento': [6.5, 4, 5.5, 3, 9, 7.2]
+}
+
+indices = ['A', 'B', 'C', 'D', 'E', 'F']
+
+tabela_filmes = pd.DataFrame(filmes, indices)
+print(tabela_filmes)
+
+print('-'*60)
+# print(tabela_filmes.iloc[0])
+print(tabela_filmes.iloc[1:3])
+print('-'*60)
+# print(tabela_filmes.loc['B'])
+print(tabela_filmes.loc['B':'E'])
+print('-'*60)
+consulta1 = tabela_filmes.query("faturamento == 6.5")
+print(consulta1)
+print('-'*60)
